89b8c2a7f9f1b42e441af3729e7daa5263228a5c
Kuriko Moe <kurikomoe@gmail.com>
2022-09-19 23:34:24 +0800

    (WIP) add windows tun support via wintun

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -17,6 +17,11 @@
 #[cfg(unix)]
 pub mod posix;
 
+#[cfg(target_os = "windows")]
+pub mod windows;
+#[cfg(target_os = "windows")]
+pub use self::windows::{create, Configuration, Device, Queue};
+
 #[cfg(target_os = "linux")]
 pub mod linux;
 #[cfg(target_os = "linux")]
